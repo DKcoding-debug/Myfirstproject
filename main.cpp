1c56d53541fd9de4102503f0dc451d7db103b61d
DKcoding-debug <kouete1@tritons.iowacentral.edu>
2024-08-29 10:38:42 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+	Delvis Kouete
+	Computer Science Fall 2024
+	Due: Sept. 3, 2024
+	Lab 1, Lab 1 Introduction to C++ Programming
+	This lab is a basic demo of C++ language.
+	*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << std::endl;
+	std::wcout << "Green" << std::endl;
+
+	return 0;
+}
